cb61b5a91b043697a1c9177c5f11dd6f733afa11
parthvaghelahd <parthvaghelahd@gmail.com>
2020-01-28 15:36:22 +0530

    http read Solution

--- program.js
+++ program.js
@@ -1,18 +1,7 @@
 require('es6-promise')
 const process = require('process')
 
-const all = (promise1, promise2) => {
-  let data = [];
-  return new Promise((resolve, reject) => {
-    return promise1.then((response) => {
-      data.push(response);
-      return promise2.then((response) => {
-        data.push(response)
-        resolve(data);
-      });
-    });
-  });
-}
-all(getPromise1(), getPromise2()).then((response) => {
-  console.log(response);
-});
+var HTTP = require("q-io/http")
+HTTP.read('http://localhost:1337')
+.then(response => JSON.parse(response))
+.then(console.log1)
