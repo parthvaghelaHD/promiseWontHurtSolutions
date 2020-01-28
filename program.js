a023a692819c374ee7ca5ce9dc60551212575669
parthvaghelahd <parthvaghelahd@gmail.com>
2020-01-28 14:53:01 +0530

    throw an error Solution

--- program.js
+++ program.js
@@ -1,10 +1,25 @@
-require('es6-promise');
+require('es6-promise')
+const process = require('process')
 
-const attachTitle = (value)=> {
-  return 'DR. ' + value
+const parsePromised = (json)=> {
+  return new Promise((resolve, reject) => {
+    try {
+      resolve(JSON.parse(json));
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
-const promise = new Promise((resolve,reject) => {
-  resolve('MANHATTAN')
+
+const onReject = (error)=> {
+  console.log(error.message);
+}
+
+
+parsePromised(process.argv[2])
+.then(null, onReject)
+.catch((e)=> {
+  throw e
 })
-.then(attachTitle)
-.then(console.log)
+
+
