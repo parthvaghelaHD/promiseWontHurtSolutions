6f7cdb15a40423e1aa61a7434681f87fa5779aa8
parthvaghelahd <parthvaghelahd@gmail.com>
2020-01-28 11:58:25 +0530

    reject a promise

--- program.js
+++ program.js
@@ -1,3 +1,17 @@
-setTimeout(()=> {
-  console.log('TIMED OUT!')
-}, 300)
+require('es6-promise');
+
+const promise = new Promise((fulfill, reject) => {
+  setTimeout(() => {
+    reject(new Error('REJECTED!'));
+  }, 300);
+});
+
+promise.then(() => {
+  console.log("done");
+}, function (error) {
+  onReject(error);
+});
+
+const onReject = (error) => {
+  console.log(error.message);
+}
