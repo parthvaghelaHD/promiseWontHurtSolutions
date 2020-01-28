d85b9d25fcae6ae77516434c0572f34835ec92f4
parthvaghelahd <parthvaghelahd@gmail.com>
2020-01-28 12:24:06 +0530

    always asynchronous Solution

--- program.js
+++ program.js
@@ -1,11 +1,8 @@
 require('es6-promise');
 
-const promises = new Promise((fulfill, reject) => {
-  fulfill('I FIRED');
-  reject(new Error('I DID NOT FIRE'));
-});
-
-function onReject(error){
-  console.log(error.message)
-}
-promises.then(console.log, onReject)
+const promise = new Promise((fulfill, reject) => {
+  fulfill('PROMISE VALUE')
+}).then((successMsg)=> {
+  console.log(successMsg)
+}, null)
+console.log('MAIN PROGRAM')
